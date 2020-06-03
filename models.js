bae6dc9be6adfc4a2d01a59fb17947e7ecc18361
Shenchin <aa254412@gmail.com>
2020-06-03 21:04:08 +0800

    model變更

--- models.js
+++ models.js
@@ -7,19 +7,49 @@ mongoose.connect("mongodb://localhost:27017/gps", {
     useUnifiedTopology: true
 });
 
-const studentSchema = mongoose.Schema({
-    id: Number,
+const userSchema = mongoose.Schema({
+    t_flag: Boolean,
+    u_id: Number,
     name: String,
     email: String,
+    t_id: Number,       //所屬的 team 的 id
+
+    d_level: Number,    //學生才擁有，系級
+
+    pro_link: String,   //老師才擁有，自介的link
 })
 
-const teacherSchema = mongoose.Schema({
-    id: Number,
-    name: String,
-    email: String,
+const commentSchema = mongoose.Schema({
+    m_id: Number,
+    message: String,
+    c_name: String,     //訊息的傳送者
+})
+
+const communicationSchema = mongoose.Schema({
+    m_id: Number,
+    message: String,
+    c_name: String,     //訊息的傳送者
+    p_link: String,     //圖片連結
+    time: String,       //訊息傳送時間
+})
+
+const teamSchema = mongoose.Schema({
+    t_id: Number,
+    g_teacher_name: String, //指導老師的名字
+    d_level: Number,        //該組別的系級
+    work_info: String,      //該組別之作品編碼
+})
+
+const remind_mailSchema = mongoose.Schema({
+    m_id: Number,
+    message: String,
+    time: String,     //要提醒user(老師)的時間
 })
 
 module.exports = {
-    student:mongoose.model('Students', studentSchema),
-    teacher:mongoose.model('Teachers', teacherSchema),
+    user:mongoose.model('Users', studentSchema),
+    comment:mongoose.model('Comments', commentSchema),
+    communication:mongoose.model('Communications', communicationSchema),
+    team:mongoose.model('Teams', teamSchema),
+    remind_mail:mongoose.model('Remind_mails', remind_mailSchema),
 }
