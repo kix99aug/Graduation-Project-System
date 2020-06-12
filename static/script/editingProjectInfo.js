a6307c13d099c23244ad68be477057c3fb1d406a
unknown <kix@laptop.kix.idv.tw>
2020-06-13 00:20:39 +0800

    auto add input

--- static/script/editingProjectInfo.js
+++ static/script/editingProjectInfo.js
@@ -1,31 +1,24 @@
-var memberNum = 0;
 var member = document.getElementById("member");
 
-member.addEventListener("keypress", addInputBar);
-member.addEventListener("click", deleteInputBar);
+$(".row.mb-2 input").on("input", onInput)
 
-function addInputBar(e) {
-  if (e.key === "Enter") {
-    var div = document.createElement("div");
-    div.setAttribute("class", "row mb-2");
-    var input = document.createElement("input");
-    input.setAttribute("type", "text");
-    input.setAttribute("style", "width: 50%;");
-    input.setAttribute("class", "form-control");
-    var i = document.createElement("i");
-    i.setAttribute("class", "far fa-times-circle");
-    i.setAttribute("style", "margin-left: -25px; margin-top: 12px;");
-    div.appendChild(input);
-    div.appendChild(i);
-    member.appendChild(div);
-    memberNum++;
+function onInput(e) {
+  if ($(this).val() == "" && $(".row.mb-2 input").length > 1) {
+    $(this).parent().remove()
+    return
+  }
+  if ($(".row.mb-2 input").last().val() != "") {
+    $(addInputBar()).on("input", onInput)
   }
 }
-function deleteInputBar(e) {
-  if (memberNum != 0 && e.target.nodeName == "I") {
-    console.log(e.target.nodeName);
-    var parentnode = e.target.parentNode;
-    member.removeChild(parentnode);
-    memberNum--;
-  }
+
+function addInputBar() {
+  var div = document.createElement("div");
+  div.setAttribute("class", "row mb-2");
+  var input = document.createElement("input");
+  input.setAttribute("type", "text");
+  input.setAttribute("class", "form-control w-50");
+  div.appendChild(input);
+  member.appendChild(div);
+  return input
 }
