9ed06c40386df44998944d92055e3e69db156893
KenPan <sheep5168947@gmail.com>
2020-06-17 00:34:17 +0800

    TAT conference

--- app.js
+++ app.js
@@ -1,264 +1,325 @@
-const path = require('path')
-const crypto = require('crypto')
+const path = require("path");
+const crypto = require("crypto");
 const fetch = require("node-fetch");
-const Koa = require('koa')
-const Router = require('koa-router')
-const views = require('koa-views')
-const serve = require('koa-static')
-const mount = require('koa-mount')
+const Koa = require("koa");
+const Router = require("koa-router");
+const views = require("koa-views");
+const serve = require("koa-static");
+const mount = require("koa-mount");
 const session = require("koa-session");
-const bodyParser = require('koa-bodyparser');
-const MongooseStore = require("koa-session-mongoose")
-const Models = require("./models")
+const bodyParser = require("koa-bodyparser");
+const MongooseStore = require("koa-session-mongoose");
+const Models = require("./models");
+const app = new Koa();
+const router = new Router();
+app.keys = [crypto.randomBytes(20).toString("hex")];
 
-const app = new Koa()
-const router = new Router()
-app.keys = [crypto.randomBytes(20).toString('hex')];
+const client_id =
+  "712826989675-rs5ej0evsmp78hsphju6sudhhn3pb38s.apps.googleusercontent.com";
+const client_secret = "zlT87D-MtpTF5ltC3w5k2hKN";
 
-const client_id = "712826989675-rs5ej0evsmp78hsphju6sudhhn3pb38s.apps.googleusercontent.com"
-const client_secret = "zlT87D-MtpTF5ltC3w5k2hKN"
-
-app.use(views(path.join(__dirname, './views'), {
-    extension: 'ejs'
-}))
+app.use(
+  views(path.join(__dirname, "./views"), {
+    extension: "ejs",
+  })
+);
 app.use(bodyParser());
 
-let notes =
-{
-    1: { x: .1, y: .1, content: "123" },
-    2: { x: .2, y: .2, content: "123" },
-    3: { x: .3, y: .3, content: "123" },
-    4: { x: .4, y: .4, content: "123" },
-}
-
+let notes = {
+  1: { x: 0.1, y: 0.1, content: "123" },
+  2: { x: 0.2, y: 0.2, content: "123" },
+  3: { x: 0.3, y: 0.3, content: "123" },
+  4: { x: 0.4, y: 0.4, content: "123" },
+};
 
 router
-    .get('/', async ctx => {
-        ctx.redirect("/intro")
-    })
-    .get('/intro', async ctx => {
-        await ctx.render("intro", {
-            title: "高雄大學資訊工程學系 畢業專題交流平台"
-        })
-    })
-    .get('/login', async ctx => {
-        var url = `https://accounts.google.com/o/oauth2/v2/auth?scope=email%20profile&redirect_uri=http://localhost:3000/loginCallback&response_type=code&client_id=${client_id}`
-        ctx.redirect(url)
-    })
-    .get('/index', async ctx => {
-        await ctx.render("index", {
-            title: "畢業專題交流平台",
-            name: ctx.session.name ? ctx.session.name : "訪客",
-            image: ctx.session.image ? ctx.session.image : "/static/images/favicon_sad.png"
-        })
-    })
-    .get('/profile', async ctx => {
-        await ctx.render("profile", {
-            title: "畢業專題交流平台",
-            name: ctx.session.name ? ctx.session.name : "訪客",
-            image: ctx.session.image ? ctx.session.image : "/static/images/favicon_sad.png",
-            grade: "避不了業",
-            professor: "沒人要你",
-            introduction: ctx.session.introduction ? ctx.session.introduction : "我是大雞雞，又香又甜又好吃"
-        })
-    })
-    .get('/projects', async ctx => {
-        await ctx.render("projects", {
-            title: "畢業專題交流平台",
-            name: ctx.session.name ? ctx.session.name : "訪客",
-            image: ctx.session.image ? ctx.session.image : "/static/images/favicon_sad.png",
-            projectName: "行車安全警示系統",
-            projectInfo: "啊我就怕被罵啊幹你娘鄵",
-
-        })
-    })
-    .get('/project/:id', async ctx => {
-        await ctx.render("project", {
-            title: "畢業專題交流平台",
-            name: ctx.session.name ? ctx.session.name : "訪客",
-            image: ctx.session.image ? ctx.session.image : "/static/images/favicon_sad.png"
-        })
-    })
-    .get("/loginCallback", async ctx => {
-        let formData = {
-            code: ctx.query.code,
-            client_id: client_id,
-            client_secret: client_secret,
-            grant_type: "authorization_code",
-            redirect_uri: "http://localhost:3000/loginCallback"
-        }
-        let json = await (
-            await fetch("https://www.googleapis.com/oauth2/v4/token", {
-                method: 'POST',
-                headers: { "Content-Type": "application/x-www-form-urlencoded" },
-                body: Object.keys(formData).map(keyName => {
-                    return encodeURIComponent(keyName) + '=' + encodeURIComponent(formData[keyName])
-                }).join('&')
-            })
-        ).json()
-        let googleData = await (
-            await fetch(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${json.access_token}`)
-        ).json()
-        if (googleData.hd === "mail.nuk.edu.tw" || googleData.hd === "go.nuk.edu.tw") {
-            // 確認資料庫
-            console.log(googleData)
-            ctx.session.login = true
-            ctx.session.id = googleData.id
-            ctx.session.name = googleData.name
-            ctx.session.image = googleData.picture
-            ctx.redirect("/index")
-        } else {
-            // 回傳錯誤
-            ctx.throw(400)
-        }
-    })
-    .get('/team/schedule', async ctx => {
-        await ctx.render("team/schedule", {
-            title: "畢業專題交流平台",
-            subtitle: "行程表",
-            name: ctx.session.name ? ctx.session.name : "訪客",
-            image: ctx.session.image ? ctx.session.image : "/static/images/favicon_sad.png"
-        })
-    })
-    .get('/team/blackboard', async ctx => {
-        await ctx.render("team/blackboard", {
-            title: "畢業專題交流平台",
-            subtitle: "留言板",
-            name: ctx.session.name ? ctx.session.name : "訪客",
-            image: ctx.session.image ? ctx.session.image : "/static/images/favicon_sad.png"
-        })
-    })
-    .get('/team/judge', async ctx => {
-        await ctx.render("team/judge", {
-            title: "畢業專題交流平台",
-            subtitle: "專題評分",
-            name: ctx.session.name ? ctx.session.name : "訪客",
-            image: ctx.session.image ? ctx.session.image : "/static/images/favicon_sad.png",
-            studentName: ctx.session.studentName ? ctx.session.studentName : "胡帥哥",
-        })
-    })
-    .get('/team/info', async ctx => {
-        await ctx.render("team/info", {
-            title: "畢業專題交流平台",
-            subtitle: "專題資訊",
-            name: ctx.session.name ? ctx.session.name : "訪客",
-            image: ctx.session.image ? ctx.session.image : "/static/images/favicon_sad.png",
-            teamMateName: ctx.session.teamMateName ? ctx.session.teamMateName : "黃翰俞",
-            guideTeacherName: ctx.session.guideTeacherName ? ctx.session.guideTeacherName : "張寶榮",
-        })
-    })
-    
-    //Backend
-    .get('/admin', async ctx => {
-        ctx.redirect("/admin/index")
-    })
-    .get('/admin/index', async ctx => {
-        await ctx.render("admin/index", {
-            title: "畢業專題交流平台",
-            name: ctx.session.name ? ctx.session.name : "訪客",
-            image: ctx.session.image ? ctx.session.image : "/static/images/favicon_sad.png"
-        })
-    })
-    .get('/admin/managePT', async ctx => {
-        await ctx.render("admin/projectAndteamManagement", {
-            title: "畢業專題交流平台",
-            subtitle: "管理專題 & 團隊",
-            name: ctx.session.name ? ctx.session.name : "訪客",
-            image: ctx.session.image ? ctx.session.image : "/static/images/favicon_sad.png"
-        })
-    })
-    .get('/admin/editPI', async ctx => {
-        await ctx.render("admin/editingProjectInfo", {
-            title: "畢業專題交流平台",
-            subtitle: "管理專題 & 團隊",
-            name: ctx.session.name ? ctx.session.name : "訪客",
-            image: ctx.session.image ? ctx.session.image : "/static/images/favicon_sad.png"
-        })
-    })
-    .get('/admin/editPF', async ctx => {
-        await ctx.render("admin/editingProjectFiles", {
-            title: "畢業專題交流平台",
-            subtitle: "管理專題 & 團隊",
-            name: ctx.session.name ? ctx.session.name : "訪客",
-            image: ctx.session.image ? ctx.session.image : "/static/images/favicon_sad.png"
-        })
-    })
-    .get('/admin/accountM', async ctx => {
-        await ctx.render("admin/accountManagement", {
-            title: "畢業專題交流平台",
-            subtitle: "管理使用者",
-            name: ctx.session.name ? ctx.session.name : "訪客",
-            image: ctx.session.image ? ctx.session.image : "/static/images/favicon_sad.png"
-        })
-    })
-    .get('/admin/editAC', async ctx => {
-        await ctx.render("admin/editingAccount", {
-            title: "畢業專題交流平台",
-            subtitle: "管理使用者",
-            name: ctx.session.name ? ctx.session.name : "訪客",
-            image: ctx.session.image ? ctx.session.image : "/static/images/favicon_sad.png"
-        })
-    })
-    .get('/admin/timeSetting', async ctx => {
-        await ctx.render("admin/time", {
-            title: "畢業專題交流平台",
-            subtitle: "系統時程設定",
-            name: ctx.session.name ? ctx.session.name : "訪客",
-            image: ctx.session.image ? ctx.session.image : "/static/images/favicon_sad.png"
-        })
-    })
-
-    // apis
-    .get('/api/blackboard/all', async ctx => {
-        ctx.body = {
-            result: true,
-            data: notes
-        }
-    })
-    .get('/api/blackboard/remove/:id', async ctx => {
-        delete notes[ctx.params.id]
-        ctx.body = {
-            result: true
-        }
-    })
-    .post('/api/blackboard/modify/:id', async ctx => {
-        notes[ctx.params.id] = ctx.request.body
-        ctx.body = {
-            result: true
-        }
-    })
-    .post('/api/blackboard/new', async ctx => {
-        let newKey = parseInt(Math.random() * Number.MAX_SAFE_INTEGER)
-        notes[newKey] = ctx.request.body
-        ctx.body = {
-            result: true,
-            id: newKey
-        }
-    })
-
-
-app.use(session({ store: new MongooseStore() }, app))
-app.use(async (ctx, next) => {
-    ctx.set('Server', 'Koa 2.12.0')
-    await next()
-})
-app.use(async (ctx, next) => {
-    try {
-        await next()
-        const status = ctx.status || 404
-        if (status === 404) {
-            ctx.throw(404)
-        }
-    } catch (err) {
-        ctx.status = err.status || 500
-        if (ctx.status != 200) {
-            await ctx.render('error')
-        }
+  .get("/", async (ctx) => {
+    ctx.redirect("/intro");
+  })
+  .get("/intro", async (ctx) => {
+    await ctx.render("intro", {
+      title: "高雄大學資訊工程學系 畢業專題交流平台",
+    });
+  })
+  .get("/login", async (ctx) => {
+    var url = `https://accounts.google.com/o/oauth2/v2/auth?scope=email%20profile&redirect_uri=http://localhost:3000/loginCallback&response_type=code&client_id=${client_id}`;
+    ctx.redirect(url);
+  })
+  .get("/index", async (ctx) => {
+    await ctx.render("index", {
+      title: "畢業專題交流平台",
+      name: ctx.session.name ? ctx.session.name : "訪客",
+      image: ctx.session.image
+        ? ctx.session.image
+        : "/static/images/favicon_sad.png",
+    });
+  })
+  .get("/profile", async (ctx) => {
+    await ctx.render("profile", {
+      title: "畢業專題交流平台",
+      name: ctx.session.name ? ctx.session.name : "訪客",
+      image: ctx.session.image
+        ? ctx.session.image
+        : "/static/images/favicon_sad.png",
+      grade: "避不了業",
+      professor: "沒人要你",
+      introduction: ctx.session.introduction
+        ? ctx.session.introduction
+        : "我是大雞雞，又香又甜又好吃",
+    });
+  })
+  .get("/projects", async (ctx) => {
+    await ctx.render("projects", {
+      title: "畢業專題交流平台",
+      name: ctx.session.name ? ctx.session.name : "訪客",
+      image: ctx.session.image
+        ? ctx.session.image
+        : "/static/images/favicon_sad.png",
+      projectName: "行車安全警示系統",
+      projectInfo: "啊我就怕被罵啊幹你娘鄵",
+    });
+  })
+  .get("/project/:id", async (ctx) => {
+    await ctx.render("project", {
+      title: "畢業專題交流平台",
+      name: ctx.session.name ? ctx.session.name : "訪客",
+      image: ctx.session.image
+        ? ctx.session.image
+        : "/static/images/favicon_sad.png",
+    });
+  })
+  .get("/loginCallback", async (ctx) => {
+    let formData = {
+      code: ctx.query.code,
+      client_id: client_id,
+      client_secret: client_secret,
+      grant_type: "authorization_code",
+      redirect_uri: "http://localhost:3000/loginCallback",
+    };
+    let json = await (
+      await fetch("https://www.googleapis.com/oauth2/v4/token", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: Object.keys(formData)
+          .map((keyName) => {
+            return (
+              encodeURIComponent(keyName) +
+              "=" +
+              encodeURIComponent(formData[keyName])
+            );
+          })
+          .join("&"),
+      })
+    ).json();
+    let googleData = await (
+      await fetch(
+        `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${json.access_token}`
+      )
+    ).json();
+    if (
+      googleData.hd === "mail.nuk.edu.tw" ||
+      googleData.hd === "go.nuk.edu.tw"
+    ) {
+      // 確認資料庫
+      console.log(googleData);
+      ctx.session.login = true;
+      ctx.session.id = googleData.id;
+      ctx.session.name = googleData.name;
+      ctx.session.image = googleData.picture;
+      ctx.redirect("/index");
+    } else {
+      // 回傳錯誤
+      ctx.throw(400);
     }
-})
-app.use(router.routes())
-app.use(mount('/static', serve('./static')))
+  })
+  .get("/team/schedule", async (ctx) => {
+    await ctx.render("team/schedule", {
+      title: "畢業專題交流平台",
+      subtitle: "行程表",
+      name: ctx.session.name ? ctx.session.name : "訪客",
+      image: ctx.session.image
+        ? ctx.session.image
+        : "/static/images/favicon_sad.png",
+    });
+  })
+  .get("/team/blackboard", async (ctx) => {
+    await ctx.render("team/blackboard", {
+      title: "畢業專題交流平台",
+      subtitle: "留言板",
+      name: ctx.session.name ? ctx.session.name : "訪客",
+      image: ctx.session.image
+        ? ctx.session.image
+        : "/static/images/favicon_sad.png",
+    });
+  })
+  .get("/team/judge", async (ctx) => {
+    await ctx.render("team/judge", {
+      title: "畢業專題交流平台",
+      subtitle: "專題評分",
+      name: ctx.session.name ? ctx.session.name : "訪客",
+      image: ctx.session.image
+        ? ctx.session.image
+        : "/static/images/favicon_sad.png",
+      studentName: ctx.session.studentName ? ctx.session.studentName : "胡帥哥",
+    });
+  })
+  .get("/team/info", async (ctx) => {
+    await ctx.render("team/info", {
+      title: "畢業專題交流平台",
+      subtitle: "專題資訊",
+      name: ctx.session.name ? ctx.session.name : "訪客",
+      image: ctx.session.image
+        ? ctx.session.image
+        : "/static/images/favicon_sad.png",
+      teamMateName: ctx.session.teamMateName
+        ? ctx.session.teamMateName
+        : "黃翰俞",
+      guideTeacherName: ctx.session.guideTeacherName
+        ? ctx.session.guideTeacherName
+        : "張寶榮",
+    });
+  })
+  .get("/team/conference", async (ctx) => {
+    await ctx.render("team/conference", {
+      title: "畢業專題交流平台",
+      subtitle: "線上會議",
+      name: ctx.session.name ? ctx.session.name : "訪客",
+      image: ctx.session.image
+        ? ctx.session.image
+        : "/static/images/favicon_sad.png",
+    });
+  })
+  //Backend
+  .get("/admin", async (ctx) => {
+    ctx.redirect("/admin/index");
+  })
+  .get("/admin/index", async (ctx) => {
+    await ctx.render("admin/index", {
+      title: "畢業專題交流平台",
+      name: ctx.session.name ? ctx.session.name : "訪客",
+      image: ctx.session.image
+        ? ctx.session.image
+        : "/static/images/favicon_sad.png",
+    });
+  })
+  .get("/admin/managePT", async (ctx) => {
+    await ctx.render("admin/projectAndteamManagement", {
+      title: "畢業專題交流平台",
+      subtitle: "管理專題 & 團隊",
+      name: ctx.session.name ? ctx.session.name : "訪客",
+      image: ctx.session.image
+        ? ctx.session.image
+        : "/static/images/favicon_sad.png",
+    });
+  })
+  .get("/admin/editPI", async (ctx) => {
+    await ctx.render("admin/editingProjectInfo", {
+      title: "畢業專題交流平台",
+      subtitle: "管理專題 & 團隊",
+      name: ctx.session.name ? ctx.session.name : "訪客",
+      image: ctx.session.image
+        ? ctx.session.image
+        : "/static/images/favicon_sad.png",
+    });
+  })
+  .get("/admin/editPF", async (ctx) => {
+    await ctx.render("admin/editingProjectFiles", {
+      title: "畢業專題交流平台",
+      subtitle: "管理專題 & 團隊",
+      name: ctx.session.name ? ctx.session.name : "訪客",
+      image: ctx.session.image
+        ? ctx.session.image
+        : "/static/images/favicon_sad.png",
+    });
+  })
+  .get("/admin/accountM", async (ctx) => {
+    await ctx.render("admin/accountManagement", {
+      title: "畢業專題交流平台",
+      subtitle: "管理使用者",
+      name: ctx.session.name ? ctx.session.name : "訪客",
+      image: ctx.session.image
+        ? ctx.session.image
+        : "/static/images/favicon_sad.png",
+    });
+  })
+  .get("/admin/editAC", async (ctx) => {
+    await ctx.render("admin/editingAccount", {
+      title: "畢業專題交流平台",
+      subtitle: "管理使用者",
+      name: ctx.session.name ? ctx.session.name : "訪客",
+      image: ctx.session.image
+        ? ctx.session.image
+        : "/static/images/favicon_sad.png",
+    });
+  })
+  .get("/admin/timeSetting", async (ctx) => {
+    await ctx.render("admin/time", {
+      title: "畢業專題交流平台",
+      subtitle: "系統時程設定",
+      name: ctx.session.name ? ctx.session.name : "訪客",
+      image: ctx.session.image
+        ? ctx.session.image
+        : "/static/images/favicon_sad.png",
+    });
+  })
 
-app.listen(3000, async e => {
-    console.log("Koa server run on http://localhost:3000/")
-})
+  // apis
+  .get("/api/blackboard/all", async (ctx) => {
+    ctx.body = {
+      result: true,
+      data: notes,
+    };
+  })
+  .get("/api/blackboard/remove/:id", async (ctx) => {
+    delete notes[ctx.params.id];
+    ctx.body = {
+      result: true,
+    };
+  })
+  .post("/api/blackboard/modify/:id", async (ctx) => {
+    notes[ctx.params.id] = ctx.request.body;
+    ctx.body = {
+      result: true,
+    };
+  })
+  .get("/api/conference", async (ctx) => {
+    ctx.body = {
+      result: true,
+      id: ctx.session.id
+    };
+  })
+  .post("/api/blackboard/new", async (ctx) => {
+    let newKey = parseInt(Math.random() * Number.MAX_SAFE_INTEGER);
+    notes[newKey] = ctx.request.body;
+    ctx.body = {
+      result: true,
+      id: newKey,
+    };
+  });
+
+app.use(session({ store: new MongooseStore() }, app));
+app.use(async (ctx, next) => {
+  ctx.set("Server", "Koa 2.12.0");
+  await next();
+});
+app.use(async (ctx, next) => {
+  try {
+    await next();
+    const status = ctx.status || 404;
+    if (status === 404) {
+      ctx.throw(404);
+    }
+  } catch (err) {
+    ctx.status = err.status || 500;
+    if (ctx.status != 200) {
+      await ctx.render("error");
+    }
+  }
+});
+
+app.use(router.routes());
+app.use(mount("/static", serve("./static")));
+
+app.listen(3000, async (e) => {
+  console.log("Koa server run on http://localhost:3000/");
+});
---
+++ static/script/conference.js
@@ -0,0 +1,88 @@
+// $.get("/api/conference", function (data) {
+//   console.log(data);
+// });
+var id
+fetch("/api/conference")
+  .then((res) => {
+    return res.json();
+  })
+  .then((json) => {
+    if (json.result) {
+      id = json.id
+    }
+  });
+console.log(id)
+const socket = io(document.location.href);
+socket.on("userin", (e) => {
+  document.querySelector("#messages").innerHTML =
+    `
+      <div class="alert alert-primary" role="alert">
+        <a href="/user/${e.uid}">${e.name}</a> 已加入公共廣場
+        </div>
+        ` + document.querySelector("#messages").innerHTML;
+});
+socket.on("userout", (e) => {
+  document.querySelector("#messages").innerHTML =
+    `
+      <div class="alert alert-secondary" role="alert">
+          <a href="/user/${e.uid}">${e.name}</a> 已離開公共廣場
+        </div>
+        ` + document.querySelector("#messages").innerHTML;
+});
+socket.on("message", (m) => {
+  console.log(new Date(m.time * 1000));
+  if(id == m.uid){
+    document.querySelector("#messages").innerHTML =
+    `
+      <div class="media m-3">
+          <a href="/user/${m.uid}"><img src="${
+      m.picture
+    }" class="mr-0 ml-auto messageicon" alt="avatar"></a>
+          <div class="media-body">
+            <h5 class="m-0"><a href="/user/${m.uid}">${m.name}</a></h5>
+            <div style="font-size:0.2rem">${new Date(
+              m.time * 1000
+            ).toLocaleString()}</div>
+            <span></span>
+          </div>
+        </div>
+      ` + document.querySelector("#messages").innerHTML;
+  document
+    .querySelector("#messages")
+    .firstElementChild.querySelector("span").innerText = m.message;
+  }
+  else{
+    document.querySelector("#messages").innerHTML =
+    `
+      <div class="media m-3">
+          <a href="/user/${m.uid}"><img src="${
+      m.picture
+    }" class="ml-0 mr-auto messageicon" alt="avatar"></a>
+          <div class="media-body">
+            <h5 class="m-0"><a href="/user/${m.uid}">${m.name}</a></h5>
+            <div style="font-size:0.2rem">${new Date(
+              m.time * 1000
+            ).toLocaleString()}</div>
+            <span></span>
+          </div>
+        </div>
+      ` + document.querySelector("#messages").innerHTML;
+  document
+    .querySelector("#messages")
+    .firstElementChild.querySelector("span").innerText = m.message;
+  }
+  
+});
+document.querySelector("#inputbox > button").onclick = () => {
+  if (document.querySelector("#input").value != "")
+    socket.send(document.querySelector("#input").value);
+  document.querySelector("#input").value = "";
+};
+let handleEnter = (event) => {
+  if (event.keyCode == 13 && !event.shiftKey) {
+    document.querySelector("#inputbox > button").click();
+    event.preventDefault();
+  }
+};
+document.querySelector("#input").addEventListener("keydown", handleEnter);
+document.querySelector("#input").addEventListener("keypress", handleEnter);
